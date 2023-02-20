777c519229a077fed0930cb8450cb422d785e880
Michael Großklaus <mail@mgrossklaus.de>
2023-02-20 14:50:34 +0100

    Fixes webmentions for reposts and replies

--- src/js/web-mentions.js
+++ src/js/web-mentions.js
@@ -1,54 +1,56 @@
 class WebMentions extends HTMLElement {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    fetch(
-      `https://webmention.io/api/mentions.jf2?target=https://www.mgrossklaus.de${this.getAttribute(
-        "path"
-      )}`
-    )
-      .then((response) => response.json())
-      .then((json) => this.render(json))
-      .catch((e) => console.error(e.toString()));
-  }
+		fetch(
+			`https://webmention.io/api/mentions.jf2?target=https://www.mgrossklaus.de${this.getAttribute(
+				"path"
+			)}`
+		)
+			.then((response) => response.json())
+			.then((json) => this.render(json))
+			.catch((e) => console.error(e.toString()));
+	}
 
-  render({ children }) {
-    if (children.length === 0) return;
+	render({ children }) {
+		if (children.length === 0) return;
 
-    const element = document.createElement("div");
+		const element = document.createElement("div");
 
-    element.classList.add("WebMentions");
+		element.classList.add("WebMentions");
 
-    element.innerHTML = `
+		element.innerHTML = `
       <h2 class="WebMentions-heading">Webmentions</h2>
       <ol class="WebMentions-list">
         ${this.getItems(children)}
       </ol>
     `;
 
-    this.appendChild(element);
-  }
+		this.appendChild(element);
+	}
 
-  getItems(children) {
-    let html = "";
+	getItems(children) {
+		let html = "";
 
-    children.forEach((webMention) => {
-      html += `
+		children.forEach((webMention) => {
+			console.log(webMention);
+			html += `
         <li class="WebMentions-item">
           ${this.getHtml({
-            author: webMention.author,
-            type: webMention["wm-property"],
-            url: webMention.url,
-          })}
+						author: webMention.author,
+						type: webMention["wm-property"],
+						url: webMention.url,
+						text: webMention.content?.html,
+					})}
         </li>
       `;
-    });
+		});
 
-    return html;
-  }
+		return html;
+	}
 
-  getHtml({ author, type, url }) {
-    return `
+	getHtml({ author, type, url, text }) {
+		return `
       <div class="WebMention">
         <div class="WebMention-header">
           <img src="${author.photo}" alt="" aria-hidden="true">
@@ -57,17 +59,25 @@ class WebMentions extends HTMLElement {
           </a>
         </div>
         <div class="WebMention-content">
-          ${this.getContent({ type, url })}
+          ${this.getContent({ type, url, text })}
         </div>
       </div>
     `;
-  }
+	}
 
-  getContent({ type, url }) {
-    if (type === "like-of") {
-      return `<p><i>liked via <a href="${url}">${url}</a></i></p>`;
-    }
-  }
+	getContent({ type, url, text }) {
+		if (type === "like-of") {
+			return `<p><i>liked via <a href="${url}">${url}</a></i></p>`;
+		}
+
+		if (type === "in-reply-to") {
+			return text;
+		}
+
+		if (type === "repost-of") {
+			return `<p><i>reposted via <a href="${url}">${url}</a></i></p>`;
+		}
+	}
 }
 
 customElements.define("mg-webmentions", WebMentions);
